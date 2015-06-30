40b3abef7bee585168f0d18090376f6ef99cf45d
Andrew Carlson <acarl005@g.ucla.edu>
2015-06-30 13:05:16 -0700

    views set up

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,23 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name interestDatingApp
- * @description
- * # interestDatingApp
- *
- * Main module of the application.
- */
-angular
-  .module('interestDatingApp', []);
+angular.module('interestDatingApp', ['ui.router'])
+
+.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
+
+  $urlRouterProvider.otherwise("/main");
+
+  $stateProvider
+  .state('main', {
+    url: '/main',
+    templateUrl: '/views/main.html',
+  })
+  .state('login', {
+    url: '/login',
+    templateUrl: '/views/login.html',
+    controller: 'LoginCtrl',
+  })
+
+
+  // $httpProvider.interceptors.push('authInterceptor');
+
+})
