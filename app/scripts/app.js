49f592788846cace11bb8dc25cb701fc2c01f48b
Andrew Carlson <acarl005@g.ucla.edu>
2015-06-30 12:17:36 -0700

    app scaffold

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name interestDatingApp
+ * @description
+ * # interestDatingApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('interestDatingApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name interestDatingApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the interestDatingApp
+ */
+angular.module('interestDatingApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
