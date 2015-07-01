e3da965026b67382550ff840970c517a8ade1518
Jason <jzhao.690@gmail.com>
2015-06-30 21:44:02 -0700

    created form for edit profile

--- app/scripts/controllers/edit.js
+++ app/scripts/controllers/edit.js
@@ -1,15 +1,13 @@
-'use strict';
-
 angular.module('interestDatingApp')
-  .controller('UserCtrl', function ($scope, $stateParams, $http, $q, API_URL) {
-    // $http.get('http://dwa-backend.herokuapp.com/users/' + $stateParams.id).success(function(data) {
-    //   $scope.user = data
-    // })
+  .controller('EditCtrl', function ($scope, $stateParams, $http, $q, API_URL) {
     var user = $http.get(API_URL + 'users/' + $stateParams.id);
     var interests = $http.get(API_URL + 'users/' + $stateParams.id + '/interests');
 
     $q.all([user, interests]).then(function(values) {
+
       $scope.user = values[0].data;
       $scope.interests = values[1].data;
     });
+
+
   });
