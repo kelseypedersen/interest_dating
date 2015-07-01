4e0936c4e54c6f31782b674b55cb655e546a6679
Andrew Carlson <acarl005@g.ucla.edu>
2015-06-30 18:42:42 -0700

    login and register work

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,11 +16,15 @@ angular.module('interestDatingApp', ['ui.router'])
     templateUrl: '/views/login.html',
     controller: 'LoginCtrl',
   })
+  .state('logout', {
+    url: '/logout',
+    controller: 'LogoutCtrl',
+  })
 
   $httpProvider.defaults.useXDomain = true;
-  delete $httpProvider.defaults.headers.common["X-Requested-With"]; 
+  delete $httpProvider.defaults.headers.common["X-Requested-With"];
   // $httpProvider.interceptors.push('authInterceptor');
 
 })
-.constant('API_URL', 'http://localhost:3000/');
+.constant('API_URL', 'http://jzs-macbook.local:3000/');
 
--- app/scripts/services/auth.js
+++ app/scripts/services/auth.js
@@ -3,15 +3,13 @@
 angular.module('interestDatingApp')
   .service('auth', function ($http, $state, API_URL, authToken) {
     var authSuccess = function(res) {
-      authToken.setToken(res.token);
+      authToken.setToken(res);
       $state.go('main');
     };
     this.login = function(user) {
       // return $http.post(API_URL + 'login', user).success(function(res) {
       //   authSuccess(res);
       // });
-
-  console.log(user)
       return $.ajax({
         url: API_URL + 'login',
         method: "POST",
@@ -19,12 +17,21 @@ angular.module('interestDatingApp')
         data: {user: user},
         crossDomain: true,
       }).success(function(res) {
-        debugger;
-        // authSuccess(res);
+        authSuccess(res);
       });
     };
+
     this.register = function(user) {
-      return $http.post(API_URL + 'register', user).success(function(res) {
+      // return $http.post(API_URL + 'register', user).success(function(res) {
+      //   authSuccess(res);
+      // });
+      return $.ajax({
+        url: API_URL + 'users',
+        method: "POST",
+        dataType: 'json',
+        data: {user: user},
+        crossDomain: true,
+      }).success(function(res) {
         authSuccess(res);
       });
     }
