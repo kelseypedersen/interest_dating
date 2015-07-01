ddae61a021e5a01b4825a7b23bb2484a42435a39
keithconroy <kconroyjr@gmail.com>
2015-07-01 11:35:05 -0700

    mvp search feature

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -1,16 +1,34 @@
 'use strict';
 
 angular.module('interestDatingApp')
-  .controller('SearchCtrl', function ($scope, $stateParams, $http, $q) {
-    $http.get('http://dwa-backend.herokuapp.com/users/').success(function(data) {
+  .controller('SearchCtrl', function ($scope, $http) {
+    $http.get('http://dwa-backend.herokuapp.com/search').success(function(data) {
       $scope.users = data
+      // debugger
+      // $scope.users.forEach(function(user) {
+      //   user.show = true;
+      // })
+      for (var person in $scope.users){
+        // console.log($scope.users[person].interests);
+        // // // if (value == "name") doSomething();
+        // $scope.users[person].interests.forEach(function(user) {
+        //   person.show = false;
+        // })
+        $scope.users[person].show = true;
+      }
     })
-    // $scope.user = $http.get('http://dwa-backend.herokuapp.com/users/' + $stateParams.id);
-    // $scope.interests = $http.get('http://dwa-backend.herokuapp.com/users/' + $stateParams.id + '/interests');
-
-    // $q.all([$scope.user, $scope.interests]).then(function(values) {
-
-    //   $scope.user = values[0].data;
-    //   $scope.interests = values[1].data;
-    // });
+    $scope.searchInterests = function(){
+      if (!$scope.query){
+        for (var person in $scope.users){
+          $scope.users[person].show = true;
+        }
+        return;
+      }
+      for (var person in $scope.users){
+        $scope.users[person].show = $scope.users[person].interests.some(function(obj){
+          return obj.name == $scope.query
+        })
+      }
+    }
   });
+
